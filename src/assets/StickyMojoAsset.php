6817bb2e2a0a1013abe207d17e326a294c7b16c5
Damian Dennis <damiandennis@gmail.com>
2014-10-06 11:23:09 +1000

    added psr4 loading. corrected path.

--- src/assets/StickyMojoAsset.php
+++ src/assets/StickyMojoAsset.php
@@ -12,7 +12,7 @@ class StickyMojoAsset extends AssetBundle
 
     public function init()
     {
-        $this->setSourcePath(__DIR__ . '/../../../bower-asset/stickymojo');
+        $this->setSourcePath(__DIR__ . '/../../../../bower-asset/stickymojo');
         $this->setupAssets('js', ['stickyMojo']);
         parent::init();
     }
