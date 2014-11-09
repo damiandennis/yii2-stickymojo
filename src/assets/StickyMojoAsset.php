dc6e7f85d20d558a91d3992b73158dd165ef4000
Damian Dennis <damiandennis@gmail.com>
2014-11-09 13:38:15 +1000

    basic functionality added.

--- src/assets/StickyMojoAsset.php
+++ src/assets/StickyMojoAsset.php
@@ -5,14 +5,14 @@
  * Time: 6:05 AM
  */
 
-namespace damiandennis\scrollingblocks;
+namespace damiandennis\stickymojo;
 
 class StickyMojoAsset extends AssetBundle
 {
 
     public function init()
     {
-        $this->setSourcePath(__DIR__ . '/../../../../bower-asset/stickymojo');
+        $this->setSourcePath(__DIR__ . '/../../../../bower/stickymojo');
         $this->setupAssets('js', ['stickyMojo']);
         parent::init();
     }
--- src/widgets/StickyMojoWidget.php
+++ src/widgets/StickyMojoWidget.php
@@ -2,6 +2,8 @@
 namespace damiandennis\stickymojo;
 
 use yii\base\Widget;
+use yii\helpers\Html;
+use yii\web\View;
 
 /**
  * Created by PhpStorm.
@@ -12,4 +14,47 @@ use yii\base\Widget;
 class StickyMojoWidget extends Widget
 {
 
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+        $this->registerPluginAssets();
+        ob_start();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function run()
+    {
+        parent::init();
+        $content = ob_get_clean();
+        $wrapper = Html::tag('div', $content, ['id' => $this->id]);
+        return $wrapper;
+
+    }
+
+    /**
+     * Registers the needed assets
+     */
+    public function registerPluginAssets()
+    {
+        $view = $this->getView();
+        StickyMojoAsset::register($view);
+        $view->registerJs(
+            "$(function() {
+                $('#restaurant-navigation').stickyMojo({
+                    footerID: '#footer',
+                    contentID: '#restaurant-menus',
+                    orientation: 'left',
+                    offsetTop: 60
+                });
+                $('#cart').stickyMojo({footerID: '#footer', contentID: '#restaurant-menus', orientation: 'right', offsetTop: 60});
+            });
+            \n",
+            View::POS_END
+        );
+    }
 }
